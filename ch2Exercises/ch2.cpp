276225e47fd57c51dce17342a39d29cbb520a72f
davidkmg1996 <dmarkbleh@outlook.com>
2024-06-24 10:25:58 -0400

    added 2.20

--- ch2Exercises/ch2.cpp
+++ ch2Exercises/ch2.cpp
@@ -11,13 +11,14 @@ int main() {
 
 	int twoSeventeen();
 	int twoEighteen();
+	int circle();
 
 	//2.17
 	twoSeventeen();
 	//2.18
 	twoEighteen();
-
-
+	//2.20
+	circle();
 
 	return 0;
 }
@@ -56,17 +57,37 @@ int twoEighteen() {
 
 	if (num1 > num2) {
 
-		cout << num1 << " is greater than " << num2 << endl;
+		cout << num1 << " is greater than " << num2 << "\n" << endl;
 	}
 
 	if (num2 > num1) {
-		cout << num2 << " is greater than " << num1 << endl;
+		cout << num2 << " is greater than " << num1 << "\n" << endl;
 	}
 
 	if (num1 == num2) {
 
-		cout << num1 << " equals " << num2 << endl;
+		cout << num1 << " equals " << num2 << "\n" << endl;
 	}
+
+	return 0;
+}
+
+int circle() {
+	
+	int pi = 3.14169;
+	int radius = 0;
+	int circumference = 0;
+	int area = 0;
+
+	cout << "Please enter the radius of a circle" << endl;
+	cin >> radius;
+
+	circumference = 2 * pi * radius;
+	area = pi * (radius * radius);
+
+	cout << "The radius of the circle is " << radius << "\n";
+	cout << "The circumference of the circle is " << circumference << "\n";
+	cout << "The area of the circle is " << area << "\n";
 
 	return 0;
 }
