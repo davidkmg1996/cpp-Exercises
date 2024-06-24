0a4f508c2e89a8f6c76437c38ef09f1845bc4ab5
davidkmg1996 <dmarkbleh@outlook.com>
2024-06-24 09:39:22 -0400

    Add project files.

---
+++ ch2Exercises/2.1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+
+	//a
+	cout << "1 2 3 4\n" << endl;
+	
+	//b
+	cout << "1 " << "2 " << "3 " << "4\n" << endl;
+
+	//c
+	cout << "1 ";
+	cout << "2 ";
+	cout << "3 ";
+	cout << "4 ";
+
+
+
+	return 0;
+}
