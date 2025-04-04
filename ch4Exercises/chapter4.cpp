7f2e478057a53e5907abea683547b4fcfbd242b5
David Markowski-Goldwyn <122569439+davidkmg1996@users.noreply.github.com>
2025-04-04 17:48:14 -0400

    Add files via upload

--- ch4Exercises/chapter4.cpp
+++ ch4Exercises/chapter4.cpp
@@ -1,1 +1,40 @@
+//
+// Created by David Markowski-Goldwyn on 4/4/25.
+//
+
+//Skipping some of chapter 3 for a while, having comp issues on macOS
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+
+ //4.3
+    int x = 0;
+    x = x + 1;
+    cout << x << endl;
+    x = 0;
+
+    x = 1 + x;
+
+    cout << x << endl;;
+
+    x = 0;
+
+    x = ++x;
+
+    cout << x << endl;
+
+    x = 0;
+
+    x += 1;
+
+    cout << x << endl;
+
+
+ }
+
 
