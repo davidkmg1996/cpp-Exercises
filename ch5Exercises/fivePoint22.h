c11bd5fa16496da8729351bd873e1fdf3d1d35a0
David Markowski-Goldwyn <122569439+davidkmg1996@users.noreply.github.com>
2025-04-11 00:15:30 +0000

    5.22

---
+++ ch5Exercises/fivePoint22.h
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+class fiveTwentyTwo {
+
+    public:
+        int x, y, a, b, g, j, i;
+        bool bleh;
+
+        void DeMorgan() {
+
+            //a
+
+            x = 6;
+            y = 6;
+
+            if (!(x < 5) && !(y >= 7)) {
+                bleh = true;
+                cout << "\n" << boolalpha << bleh << "\n";
+            } else {
+                bleh = false;
+            }
+
+            if ((x >= 5) && (y < 7)) {
+                bleh = true;
+                cout << "\n" << boolalpha << bleh << "\n";
+            } else {
+                bleh = false;
+            }
+
+            a = 1;
+            b = 2;
+            g = 5;
+
+            //b
+            if (!(a == b) || !(g != 5)) {
+                bleh = true;
+                cout << "\n" << boolalpha << bleh << "\n";
+            } else {
+                bleh = false;
+            }
+
+            if (!(a == b && g != 5)) {
+                bleh = true;
+                cout << "\n" << boolalpha << bleh << "\n";
+            } else {
+                bleh = false;
+            }
+
+            x = 9;
+            y = 3;
+
+            //c
+            if (!((x <= 8) && (y > 4))) {
+                bleh = true;
+                cout << "\n" << boolalpha << bleh << "\n";
+            } else {
+                bleh = false;
+            }
+
+            if (x > 8 || y <= 4) {
+                bleh = true;
+                cout << "\n" << boolalpha << bleh << "\n";
+            } else {
+                bleh = false;
+            }
+            
+            //d
+            i = 3;
+            j = 7;
+
+            if (!((i > 4 ) || (j <= 6))) {
+                bleh = true;
+                cout << "\n" << boolalpha << bleh << "\n";
+            } else {
+                bleh = false;
+            }
+
+            if ((i <= 4) && (j > 6)) {
+                bleh = true;
+                cout << "\n" << boolalpha << bleh << "\n";
+            } else {
+                bleh = false;
+            }
+
+        }
+
+
+};
